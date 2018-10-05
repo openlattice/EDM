2ae7234174a195324403a243018ddd3edb6f2dd8
jokedurnez <joke.durnez@gmail.com>
2018-10-05 11:02:34 -0700

    push grabbing/writing edm to utilities

--- edm-controller/grab_edm.py
+++ edm-controller/grab_edm.py
@@ -1,4 +1,3 @@
-import yamlordereddictloader
 import openapi_client
 import json
 import yaml
@@ -6,39 +5,7 @@ import os
 
 from utils import utils
 
-basedir = os.path.dirname(os.path.realpath(__file__))
-datadir = os.path.abspath(os.path.join(basedir,"..","data"))
 
-# Configure API key authorization
-baseurl = 'https://api.openlattice.com'
+edm = utils.grab_edm()
 
-configuration = openapi_client.Configuration()
-configuration.host = baseurl
-
-api_instance = openapi_client.EdmApi(openapi_client.ApiClient(configuration))
-
-# get and process property types
-properties = api_instance.get_all_property_types()
-props = [utils.get_human_property(x.to_dict()) for x in properties]
-propfile = os.path.join(datadir,'propertytypes.yaml')
-with open(propfile, 'w') as outfile:
-    yaml.dump(props, outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
-
-propdict = {x.to_dict()['id']:".".join(x.to_dict()['type'].values()) for x in properties}
-
-# get and process entity types
-entities = api_instance.get_all_entity_types()
-ents = [utils.get_human_entity(x.to_dict(), propdict) for x in entities]
-entfile = os.path.join(datadir,'entitytypes.yaml')
-with open(entfile, 'w') as outfile:
-    yaml.dump(ents, outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
-
-entdict = {x.to_dict()['id']:".".join(x.to_dict()['type'].values()) for x in entities}
-
-
-# get and process association types
-associations = api_instance.get_all_association_entity_types()
-ass = [utils.get_human_association(x.to_dict(), propdict, entdict) for x in associations]
-assfile = os.path.join(datadir,'associationtypes.yaml')
-with open(assfile, 'w') as outfile:
-    yaml.dump(ass, outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
+utils.write_edm(edm)
--- edm-controller/utils/utils.py
+++ edm-controller/utils/utils.py
@@ -1,8 +1,51 @@
 from collections import OrderedDict
+import yamlordereddictloader
 import openapi_client
 import copy
+import yaml
 import json
 import os
+
+def grab_edm():
+    # Configure API key authorization
+    baseurl = 'https://api.openlattice.com'
+    configuration = openapi_client.Configuration()
+    configuration.host = baseurl
+    api_instance = openapi_client.EdmApi(openapi_client.ApiClient(configuration))
+
+    edm = {}
+    # get and process property types
+    properties = api_instance.get_all_property_types()
+    edm['properties'] = [get_human_property(x.to_dict()) for x in properties]
+    propdict = {x.to_dict()['id']:".".join(x.to_dict()['type'].values()) for x in properties}
+
+    # get and process entity types
+    entities = api_instance.get_all_entity_types()
+    edm['entities'] = [get_human_entity(x.to_dict(), propdict) for x in entities]
+    entdict = {x.to_dict()['id']:".".join(x.to_dict()['type'].values()) for x in entities}
+
+    # get and process association types
+    associations = api_instance.get_all_association_entity_types()
+    edm['associations'] = [get_human_association(x.to_dict(), propdict, entdict) for x in associations]
+
+    return edm
+
+def write_edm(edm):
+    basedir = os.path.dirname(os.path.realpath(__file__))
+    datadir = os.path.abspath(os.path.join(basedir,"../..","data"))
+
+    propfile = os.path.join(datadir,'propertytypes.yaml')
+    entfile = os.path.join(datadir,'entitytypes.yaml')
+    assfile = os.path.join(datadir,'associationtypes.yaml')
+
+    with open(propfile, 'w') as outfile:
+        yaml.dump(edm['properties'], outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
+    with open(entfile, 'w') as outfile:
+        yaml.dump(edm['entities'], outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
+    with open(assfile, 'w') as outfile:
+        yaml.dump(edm['associations'], outfile, Dumper=yamlordereddictloader.Dumper,indent=4)
+    return 0
+
 
 def type_to_fqn(type):
     fqn = "%s.%s"%(type['namespace'], type['name'])
