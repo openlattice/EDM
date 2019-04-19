e5319b8f27587cee1264c6b46e27370fd0a29099
jokedurnez <joke.durnez@gmail.com>
2019-04-19 12:38:36 -0700

    indentations of report

--- controller/utils/commenter.py
+++ controller/utils/commenter.py
@@ -11,21 +11,19 @@ def comment(pull, master_key, pull_key, prod_key):
     prod_report = changes.changeReport(master_key, prod_key).get_report()
     
     report = ""
-    if len(prod_report) > 0:
-        report += '''
-                 # Changes from prod
-                 Somebody made changes to prod between the previous commit and this PR.  We merged in those changes safely, but we need to avoid this from happening.
-                 {prod_report}
-                 '''.format(prod_report=prod_report)
+    if len(prod_report.replace(" ", "").replace("\n", "")) > 0:
+        report += '''# Changes from prod
+Somebody made changes to prod between the previous commit and this PR.  We merged in those changes safely, but we need to avoid this from happening.
+{prod_report}
+'''.format(prod_report=prod_report)
         prod = True
     else:
         prod = False
         
-    report += '''
-            # New changes
+    report += '''# New changes
     
-            {pr_report}
-            '''.format(pr_report = pr_report)
+{pr_report}
+'''.format(pr_report = pr_report)
 
     report = report.replace("  "," ").replace("\n\n", "\n")
     pull.create_issue_comment(report)
